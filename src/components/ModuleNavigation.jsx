33d960a167fdfa6bbb5226b4f47654fe23d0fc29
arennaoui-debug <arennaoui5@gmail.com>
2025-10-23 11:31:32 +0200

    Update ModuleNavigation.jsx

--- src/components/ModuleNavigation.jsx
+++ src/components/ModuleNavigation.jsx
@@ -6,7 +6,9 @@ import { navModules } from '../data/mockData';
 const ModuleNavigation = () => {
   const OtacosLogo = () => (
     <div className="flex items-center justify-center p-4 text-white">
-     <FaIcons.FaTachometerAlt className="text-otacos-accent text-3xl mr-2" />
+      {/* CORRECTION 1: Remplacement de FaTaco (qui n'existe pas) par une icône sûre, FaTruck. */}
+      {/* Note : Vous pouvez aussi essayer FaBurger si vous l'avez, mais FaTruck est plus générique pour un hub. */}
+      <FaIcons.FaTruck className="text-otacos-accent text-3xl mr-2" /> 
       <span className="text-xl font-bold">O'Tacos-CONNECT</span>
     </div>
   );
@@ -16,8 +18,9 @@ const ModuleNavigation = () => {
       <OtacosLogo />
       <nav className="flex-grow p-4 space-y-2">
         {navModules.map((item) => {
-          // Important: Récupère l'icône par son nom de chaîne (e.g., 'FaUsers')
+          // Récupère l'icône par son nom (e.g., 'FaUsers')
           const IconComponent = FaIcons[item.icon]; 
+          
           return (
             <NavLink
               key={item.path}
@@ -30,8 +33,9 @@ const ModuleNavigation = () => {
                 }`
               }
             >
-              {/* S'assure que l'icône existe avant de l'afficher */}
-              {IconComponent && <IconComponent className="w-5 h-5 mr-3" />}
+              {/* CORRECTION 2: Vérification de l'existence du composant IconComponent */}
+              {IconComponent ? <IconComponent className="w-5 h-5 mr-3" /> : <FaIcons.FaQuestionCircle className="w-5 h-5 mr-3" />}
+              
               {item.name}
             </NavLink>
           );
@@ -42,4 +46,3 @@ const ModuleNavigation = () => {
 };
 
 export default ModuleNavigation;
-
