31c36f38011e4ca7767d53051df765a706bc7f58
arennaoui-debug <arennaoui5@gmail.com>
2025-10-23 11:55:46 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 // vite.config.js
-// vite.config.js
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Ceci est la correction pour les problèmes de chargement des ressources (page blanche)
-  base: '/', 
+  // Change base from '/' to './' to fix relative path issues
+  base: './', 
 });
