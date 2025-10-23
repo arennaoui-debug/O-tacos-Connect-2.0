5ad0b79d49041ec91efe94851f8ba20414eb25d2
arennaoui-debug <arennaoui5@gmail.com>
2025-10-23 11:53:05 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 // vite.config.js
+// vite.config.js
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -6,6 +7,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Ajoutez cette ligne pour le chemin de base Vercel/GitHub Pages
+  // Ceci est la correction pour les problèmes de chargement des ressources (page blanche)
   base: '/', 
 });
